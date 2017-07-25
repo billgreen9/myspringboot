9bd6b71c9ffa49655cb2be86452919faf8baedf4
billgreen9 <liuquan899@qq.com>
2017-07-25 17:06:16 +0800

    代码提交，添加代码多环境支持

--- src/main/java/com/person/myspringboot/Config.java
+++ src/main/java/com/person/myspringboot/Config.java
@@ -1,7 +1,40 @@
 package com.person.myspringboot;
 
-public class Config {
-	
-	
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-}
+@Configuration
+@ConfigurationProperties(prefix="hello")
+public class Config {
+
+	private String name;
+	private String url;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getUrl() {
+		return url;
+	}
+
+
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+
+	public String getInfo(){
+		return "name="+name+",url="+url;
+	}
+   
+
+} 
--- src/main/java/com/person/myspringboot/SampleController.java
+++ src/main/java/com/person/myspringboot/SampleController.java
@@ -1,5 +1,6 @@
 package com.person.myspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SampleController {
 
-	@RequestMapping("/sample")
+	@Autowired
+	private Config config;
+	
+	@Value("${name}")
+	private String name;
+
+	@RequestMapping("/hello")
 	@ResponseBody
 	public String home() {
-		return "Hello World!" ;
+		return "Hello World!" + config.getInfo()+"<br/>|||||name="+name;
 	}
 
 }
